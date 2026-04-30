d00eb21c95398010b77713f12cb0ef2aa67a6175
Charina Mae R. Castillano <crcastillano@davao-water.gov.ph>
2026-04-30 09:28:00 +0800

    Change File name to k6_test and added API from ERP Billing and Collection

--- bulkapi.js
+++ bulkapi.js
@@ -5,34 +5,59 @@ import { sleep } from "k6";
 const FAST_LIMIT_MS = 500;
 const WARNING_LIMIT_MS = 1000;
 
-const BEARER_TOKEN = "test123456789";
+const BEARER_TOKEN = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI2YmE5M2Y5Zi0yZDRhLTQ1MDctOTI1OS02ZmQ0MzYyMTAyYzgiLCJzdWIiOiIyIiwibmFtZSI6IjAwMTE1OCIsInJvbGUiOiJCaWxsaW5nIEFkbWluIiwibmJmIjoxNzc3NTA1OTA4LCJleHAiOjE3NzgxMTA3MDgsImlhdCI6MTc3NzUwNTkwOCwiaXNzIjoiZXJwQkNBQVBJIiwiYXVkIjoiZXJwQkNBQVBJIn0.3uYY4AHltaFDEDhN2Tg-TXJ2O97kvwrM0-QYne3XAH0ctSJ5wAm1CQliG6E8tURvXIqfCO6kLmCfLec6FrDRLA";
 
 const apis = [
   {
-    name: "JSONPlaceholder Posts",
+    name: "Bill Calculator",
     method: "GET",
-    url: "https://jsonplaceholder.typicode.com/posts",
+    url: "https://dev-api3.davao-water.gov.ph/dcwd-erp-billing-collection/api/v1/billing/bill-calculator?accountNumber=051649713&ratesKey=02-01-2014&meterMaintenanceChargeFlag=1&consumption=25",
   },
   {
-    name: "JSONPlaceholder Users",
-    method: "GET",
-    url: "https://jsonplaceholder.typicode.com/users",
-  },
-  {
-    name: "HTTPBin Bearer Test",
-    method: "GET",
-    url: "https://httpbin.org/bearer",
-  },
-  {
-    name: "HTTPBin Get With Params",
-    method: "GET",
-    url: "https://httpbin.org/get?customerId=000001&type=test",
-  },
-  {
-    name: "Restful Booker Ping",
-    method: "GET",
-    url: "https://restful-booker.herokuapp.com/ping",
+    name: "BillingAdjustmentMemo-CreationBulkBamRequest",
+    method: "POST",
+    url: "https://dev-api3.davao-water.gov.ph/dcwd-erp-billing-collection/api/v1/billing/bam/create-bulk-bam-request",
+    body: {
+    documentNumberType: 1,
+    requests: [
+      {
+        bamRequestType: 1,
+        jmsId: 0,
+        documentId: 0,
+        documentNumber: "string",
+        highConsumptionAmount: 0,
+        averageConsumptionAmount: 0,
+        meteredSaleAmount: 0,
+        penaltyAmount: 0,
+        adjustmentAmount: 0,
+        transactionId: 0,
+        contraTransactionId: 0,
+        asBilledAmount: 0,
+        shoudBeAmount: 0,
+        actionRecommended: "string",
+        remarks: "string",
+        requestedBy: "string",
+        contactNumber: "string",
+        preparedBy: "string"
+      }
+    ]
   }
+},
+  {
+    name: "BillingAdjustmentMemo-CreateLeakAdjustment",
+    method: "POST",
+    url: "https://dev-api3.davao-water.gov.ph/dcwd-erp-billing-collection/api/v1/billing/bam/create-leak-adjustment",
+   },
+  // {
+  //   name: "HTTPBin Get With Params",
+  //   method: "GET",
+  //   url: "https://httpbin.org/get?customerId=000001&type=test",
+  // },
+  // {
+  //   name: "Restful Booker Ping",
+  //   method: "GET",
+  //   url: "https://restful-booker.herokuapp.com/ping",
+  // }
 ];
 
 export const options = {
